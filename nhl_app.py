8a349426162a39ac40687c38b3ffc55f1b9c0310
Jeremy <63766552+jeremybutt09@users.noreply.github.com>
2025-04-24 21:57:31 -0600

    Update nhl_app.py

--- nhl_app.py
+++ nhl_app.py
@@ -132,8 +132,9 @@ else:
                                    'visitingScore': 'Visiting Score', 
                                    'homeTeamFullName': 'Home Team',
                                    'homeScore': 'Home Score'}, inplace=True)
-        
-        # st.dataframe(filtered_df)
+
+        st.dataframe(period_df)
+        st.dataframe(filtered_df)
         st.dataframe(display_df)
     else:
         st.warning(f"No games scheduled for {selected_date}.")
