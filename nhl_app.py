5d8a4e7ee40d31d3d85b1101d218116249889c22
Jeremy <63766552+jeremybutt09@users.noreply.github.com>
2025-04-25 23:35:35 -0600

    Update nhl_app.py
    sorting by date desc

--- nhl_app.py
+++ nhl_app.py
@@ -131,7 +131,10 @@ else:
             'visitingTeamFullName', 'visitingScore',
             'homeTeamFullName', 'homeScore'
         ]
+        
         display_df = filtered_df[display_cols] if all(col in filtered_df.columns for col in display_cols) else filtered_df
+
+        df.sort_values('easternStartTime', inplace = True)
 
         display_df.rename(columns={'easternStartTime': 'Game Date', 
                                    'periodOutput': 'Period',
